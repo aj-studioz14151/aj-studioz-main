0364f9a4d123cb248f452aea8d52628d69fe5d79
unknown <mbhuvimbhuvi72@gmail.com>
2026-02-08 15:54:28 +0530

    fix: Remove 'use client' from dynamic component page - generateStaticParams requires server component

--- app/components/[category]/[component]/page.tsx
+++ app/components/[category]/[component]/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { notFound } from 'next/navigation'
 import { componentMetadata } from '@/lib/components-metadata'
 import { ComponentTabs } from '@/components/component-tabs'
